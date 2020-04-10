46b821b0ba467ea5e8805d9a479f6178bca31247
Martin-Sparks <martin.sparks82@gmail.com>
2020-04-10 16:42:50 +0100

    Customer added to room

--- specs/karaoke_bar_specs.rb
+++ specs/karaoke_bar_specs.rb
@@ -4,6 +4,7 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../karaoke_bar')
 require_relative('../customer')
+require_relative('../room')
 
 class TestKaraokeBar < Minitest::Test
 
@@ -32,25 +33,38 @@ class TestKaraokeBar < Minitest::Test
   def test_name2
     assert_equal("Gill", @Customer1.name())
   end 
+
+  def test_room_space
+    
+  end 
   
+
+  def test_can_add_customer_to_room
+    @room.add_customer(@Customer1)
+    assert_equal("Gill", @KaraokeBar.rooms)
+  end 
+
+# def test_can_add_drinks
+#   @pub.add_drink(@drink1)
+#   assert_equal(1, @pub.drink_count)
+# end
+
+
+
+
+
+
+#   @earth = Planet.new("Earth", 12756, 149.6, 1)
+#   @jupiter = Planet.new("Jupiter", 142796, 778.3, 67)
+#   @mars = Planet.new("Mars", 6787, 227.9, 2)
+#   @mercury = Planet.new("Mercury", 4878, 57.9, 0)
+#   @neptune = Planet.new("Neptune", 48600, 4497.1, 13)
+#   @pluto = Planet.new("Pluto", 2274, 5913, 4)
+#   @saturn = Planet.new("Saturn", 120660, 1427.0, 62)
+#   @uranus = Planet.new("Uranus", 51118, 2871.0, 27)
+#   @venus = Planet.new("Venus", 12104, 108.2, 0)
+
+#   @planets = [@earth, @jupiter, @mars, @mercury, @neptune, @pluto, @saturn, @uranus, @venus]
+#   @solar_system = StarSystem.new("Solar System", @planets)
+
 end 
-
-
-
-
-=begin
-def setup
-  @earth = Planet.new("Earth", 12756, 149.6, 1)
-  @jupiter = Planet.new("Jupiter", 142796, 778.3, 67)
-  @mars = Planet.new("Mars", 6787, 227.9, 2)
-  @mercury = Planet.new("Mercury", 4878, 57.9, 0)
-  @neptune = Planet.new("Neptune", 48600, 4497.1, 13)
-  @pluto = Planet.new("Pluto", 2274, 5913, 4)
-  @saturn = Planet.new("Saturn", 120660, 1427.0, 62)
-  @uranus = Planet.new("Uranus", 51118, 2871.0, 27)
-  @venus = Planet.new("Venus", 12104, 108.2, 0)
-
-  @planets = [@earth, @jupiter, @mars, @mercury, @neptune, @pluto, @saturn, @uranus, @venus]
-  @solar_system = StarSystem.new("Solar System", @planets)
-end
-=end
--- specs/songs_specs.rb
+++ specs/songs_specs.rb
@@ -8,6 +8,17 @@ class TestSongs < Minitest::Test
 
   def setup
     @song1 = Song.new("Wonderwall", "Oasis")
+    @song2 = Song.new("Song 2", "Blur")
+    @song3 = Song.new("Let it go", "Frozen")
+    @song4 = Song.new("Shake it off", "Taylor Swift")
+    @song5 = Song.new("Summer Night", "Grease")
+    @song7 = Song.new("Livin' On A Prayer", "Bon Jovi")
+    @song8 = Song.new("Gangnam Style", "PSY")
+    @song9 = Song.new("Wonderwall", "Oasis")
+    @song10 = Song.new("Wonderwall", "Oasis")
+    @song11 = Song.new("Wonderwall", "Oasis")
+    @song12 = Song.new("Wonderwall", "Oasis")
+
 
   end
     def test_name
